23d50d7c85d08a69040b64fcc577dd4735e5450e
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2025-05-21 17:38:02 +0530

    added react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Landing from "./component/landing";
-import './index.css';
+import { Routes, Route } from "react-router-dom";
+import Hero from "./pages/Hero";
 import Nav from "./component/Nav";
-
+import "./index.css";
 
 function App() {
-  
-
   return (
     <div>
       <Nav/>
-      <Landing/>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Landing from "../component/landing";
+function Hero() {
+  return (
+    <div>
+      <Landing/>
+    </div>
+  )
+}
+
+export default Hero
