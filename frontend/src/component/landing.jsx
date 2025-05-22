0727b2832afba0c812aecd7fde1dad71e4c6e466
Aryan <arynkr07@gmail.com>
2025-05-22 21:48:10 +0530

    added  some animation and personal files

--- frontend/src/component/landing.jsx
+++ frontend/src/component/landing.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { assets } from '../assets/assets';
 
 export default function Landing() {
+  const titles = [' "Web Developer" ', ' "Gamer" ',' "GOD" '];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % titles.length);
+    }, 1000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div className="relative ml-64 w-[calc(100vw-16rem)] h-screen overflow-hidden">
       <img
@@ -9,11 +20,15 @@ export default function Landing() {
         alt="background"
         className="absolute inset-0 w-full h-full object-cover -z-10"
       />
-      
-      <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white text-5xl font-bold bg-black bg-opacity-50 p-6 rounded-lg">
+
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+        <h1 className=" text-5xl font-bold text-white">
           Welcome to My Page
         </h1>
+
+        <p className="text-white text-4xl mt-6 font-semibold">
+          I am <span className="text-blue-400 transition-opacity duration-500">{titles[index]}</span>
+        </p>
       </div>
     </div>
   );
