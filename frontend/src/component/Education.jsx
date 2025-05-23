e013740ae4adeeaadc3b8a22e0950bac3f439386
Aryan <arynkr07@gmail.com>
2025-05-23 15:46:30 +0530

    education page done

--- frontend/src/component/Education.jsx
+++ frontend/src/component/Education.jsx
@@ -1,21 +1,31 @@
 import { assets } from "../assets/assets.jsx"
 function Education() {
     return (
-        <div className="relative ml-65 w-[calc(100vw-16rem)] h-screen overflow-hidden">
+        <div className="relative ml-65 w-[calc(100vw-16rem)] min-h-screen overflow-auto">
             <>
                 <h1 className=" text-6xl flex  text-blue-400 underline ">Education</h1>
-                <h2 className="text-blue-900 my-4 text-4xl">High School</h2>
+                <h2 className="text-blue-900 my-4 text-4xl underline">High School</h2>
                 <div className="flex items-start gap-6 my-6">
-                    {/* School Logo */}
                     <img
                         src={assets.personal_assets.scl_logo}
                         alt="School logo"
                         className="w-40 h-40 border-4 border-white object-cover"
                     />
-
-                    {/* School Details */}
                     <div>
                         <h2 className="text-4xl text-blue-950">{assets.personal_assets.myschool}</h2>
+
+                        <p className="mt-2">
+                            Website:{" "}
+                            <a href={assets.personal_assets.collegewebsite} target="blank" rel="nooper noreferrer" className="text-blue-600 underline hover:text-blue-800">
+                                {assets.personal_assets.collegewebsite}
+                            </a>
+                        </p>
+                        <p>
+                            Email: {" "}
+                            <a href={assets.personal_assets.sclmail} target="blank" rel="nooper noreferrer" className="text-blue-600 underline hover:text-blue-800">
+                                {assets.personal_assets.sclmail}
+                            </a>
+                        </p>
 
                         <hr className="my-2 border-t-2 border-blue-200 w-3/4" />
                         <p className="text-2xl text-blue-950 underline">10th</p>
@@ -34,7 +44,7 @@ function Education() {
                 </div>
 
 
-                <h2 className="text-blue-900 my-4 text-4xl">Bachelor's</h2>
+                <h2 className="text-blue-900 my-4 text-4xl underline">Bachelor's</h2>
 
                 <div className="flex items-start gap-6 my-6">
                     <img
@@ -43,7 +53,27 @@ function Education() {
                         className="w-40 h-40 border-4 border-white object-cover"
                     />
                     <div>
-                        <h2 className="text-4xl text-blue-950">{assets.personal_assets.myuniversity}</h2>
+                        <h2 className="text-4xl text-blue-950">{assets.personal_assets.myuniversity} </h2>
+                        <p className="mt-2">
+                            Website:{" "}
+                            <a
+                                href={assets.personal_assets.collegewebsite}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-600 underline hover:text-blue-800"
+                            >
+                                {assets.personal_assets.collegewebsite}
+                            </a>
+                        </p>
+                        <p>
+                            Email:{" "}
+                            <a
+                                href={`mailto:${assets.personal_assets.collegemaill}`}
+                                className="text-blue-600 underline hover:text-blue-800"
+                            >
+                                {assets.personal_assets.collegemail}
+                            </a>
+                        </p>
 
                         <hr className="my-2 border-t-2 border-blue-200 w-3/4" />
 
