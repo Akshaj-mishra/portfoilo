13cd5186db9ac417db634df0a467f993e545e551
Aryan <arynkr07@gmail.com>
2025-05-23 12:37:59 +0530

    made space for contact

---
+++ frontend/src/component/Contact.jsx
@@ -0,0 +1,14 @@
+import assets from "../assets/assets.jsx"
+function Contact(){
+    return(
+        <div className="relative ml-64 w-[calc(100vw-16rem)] h-screen overflow-hidden">
+            <>
+            <h1 className=" text-6xl flex  text-blue-400 underline ">Contact</h1>
+            <h2 className="text-blue-900 my-4 text-4xl">gimmeyr no</h2>
+            </>
+        </div>
+
+    );
+}
+
+export default Contact;
--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Landing from "../component/landing";
 import Education from "../component/Education";
+import Contact from "../component/Contact";
 function Hero() {
   return (
     <div>
       <Landing/>
       <Education/>
+      <Contact/>
     </div>
   )
 }
