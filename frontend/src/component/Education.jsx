8428039486ef84c849c4237b8cac736b65c73dff
Aryan <arynkr07@gmail.com>
2025-05-23 15:09:53 +0530

    fixed a bug and education part

--- frontend/src/component/Education.jsx
+++ frontend/src/component/Education.jsx
@@ -1,36 +1,60 @@
-import assets from "../assets/assets.jsx"
-function Education(){
-    return(
+import { assets } from "../assets/assets.jsx"
+function Education() {
+    return (
         <div className="relative ml-65 w-[calc(100vw-16rem)] h-screen overflow-hidden">
             <>
-            <h1 className=" text-6xl flex  text-blue-400 underline ">Education</h1>
-            <h2 className="text-blue-900 my-4 text-4xl">High School</h2>
-            <h2 className="text:4xl text-blue-950">{assets.personal_assets.myschool}</h2>
-            <img src={assets.personal_assets.scl_logo} alt = "scl logo here"
-              className="w-38 h-38  border-4 border-white mb-4 object-cover"  />
-            <ul>
-                <p className="text-2xl text-blue-950 unerline ">10th</p>
-                <ol>
-                    <li>percentage:  {assets.personal_assets.tenth_percentage}</li>
-                    <li>duration:  {assets.personal_assets.tnth_scltiming}</li>
-                </ol>
-                <p className="text-2xl text-blue-950 unerline ">12th</p>
-                <ol>
-                    <li>percentage:  {assets.personal_assets.twelth_percentage}</li>
-                    <li>duration:  {assets.personal_assets.twlth_scltiming}</li>
-                </ol>   
-            </ul>
-            <h2 className="text-blue-900 my-4 text-4xl">Bachelor's</h2>
-            <img src={assets.public_assets.vitblogo} alt ="college logo"/>
-            <h2 className="text:4xl text-blue-950">{assets.personal_assets.myuniversity}</h2>
-                <ul>
-                <ol>
-                    <li>duration:  {assets.personal_assets.current_}</li>
-                    <li>Course:  {assets.personal_assets.current_course}</li>
-                    <li>duration:  {assets.personal_assets.current_cgpa}</li>
+                <h1 className=" text-6xl flex  text-blue-400 underline ">Education</h1>
+                <h2 className="text-blue-900 my-4 text-4xl">High School</h2>
+                <div className="flex items-start gap-6 my-6">
+                    {/* School Logo */}
+                    <img
+                        src={assets.personal_assets.scl_logo}
+                        alt="School logo"
+                        className="w-40 h-40 border-4 border-white object-cover"
+                    />
 
-                </ol>
-                </ul>
+                    {/* School Details */}
+                    <div>
+                        <h2 className="text-4xl text-blue-950">{assets.personal_assets.myschool}</h2>
+
+                        <hr className="my-2 border-t-2 border-blue-200 w-3/4" />
+                        <p className="text-2xl text-blue-950 underline">10th</p>
+                        <ol className="ml-4 mb-4">
+                            <li>Percentage: {assets.personal_assets.tenth_percentage}%</li>
+                            <li>Year: {assets.personal_assets.tnth_scltiming}</li>
+                        </ol>
+
+                        <hr className="my-2 border-t-2 border-blue-200 w-3/4" />
+                        <p className="text-2xl text-blue-950 underline">12th</p>
+                        <ol className="ml-4">
+                            <li>Percentage: {assets.personal_assets.twelth_percentage}%</li>
+                            <li>Year: {assets.personal_assets.twlth_scltiming}</li>
+                        </ol>
+                    </div>
+                </div>
+
+
+                <h2 className="text-blue-900 my-4 text-4xl">Bachelor's</h2>
+
+                <div className="flex items-start gap-6 my-6">
+                    <img
+                        src={assets.public_assets.vitblogo}
+                        alt="college logo"
+                        className="w-40 h-40 border-4 border-white object-cover"
+                    />
+                    <div>
+                        <h2 className="text-4xl text-blue-950">{assets.personal_assets.myuniversity}</h2>
+
+                        <hr className="my-2 border-t-2 border-blue-200 w-3/4" />
+
+                        <ul className="ml-4">
+                            <li className="mb-2">Duration: {assets.personal_assets.timing}</li>
+                            <li className="mb-2">Course: {assets.personal_assets.current_course}</li>
+                            <li>Current CGPA: {assets.personal_assets.current_cgpa}</li>
+                        </ul>
+                    </div>
+                </div>
+
             </>
         </div>
 
