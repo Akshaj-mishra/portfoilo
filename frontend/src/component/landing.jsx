7bd3dde62dcfea8f7a6af7cc3f22437f83e1221d
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2025-05-23 08:40:00 +0530

    updates

--- frontend/src/component/landing.jsx
+++ frontend/src/component/landing.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 import { assets } from '../assets/assets';
 
 export default function Landing() {
-  const titles = ['Web Developer', 'Student', 'Software Developer'];
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % titles.length);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const titles = ['Student .', 'Web Developer .', 'Software Developer .'];
 
   return (
     <div className="relative ml-64 w-[calc(100vw-16rem)] h-screen overflow-hidden">
@@ -23,19 +15,27 @@ export default function Landing() {
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
         <h1 className="text-5xl font-bold text-white">
-          Hello, I'm {' '}
+          Hello, I'm{' '}
           <span className="text-blue-300 transition-opacity duration-500">
             {assets.personal_assets.myname}
           </span>
         </h1>
+
         <p className="text-white text-4xl mt-6 font-semibold">
           I am{' '}
-          <span className="text-blue-400 transition-opacity duration-500">
-            {titles[index]}.
+          <span className="text-white underline decoration-blue-400 underline-offset-4">
+            <Typewriter
+              words={titles }
+              loop={true}
+              cursor
+              cursorStyle="|"
+              typeSpeed={100}
+              deleteSpeed={90}
+              delaySpeed={1000}
+            />
           </span>
         </p>
       </div>
     </div>
   );
 }
-
